f6f6a1fbb52f43be94606963206004324416d22f
Mel Jack <melissa_jack@hotmail.co.uk>
2021-07-01 23:22:18 +0100

    Timeblocks determine if past, present or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 const currentDayEl = $('#currentDay');
 
-const now = moment().format("dddd, MMMM Do");
-currentDayEl.text(now);
+const now = moment();
+currentDayEl.text(now.format("dddd, MMMM Do"));
+
+$("textarea").each(function() {
+    const format = 'H';
+    const id = $(this).parent().attr('id');
+    // Remove non-numerical characters from id to get time
+    // Convert to moment objects in 'H' format 
+    const timeblockHour = moment(id.replace(/\D/g, ''), format);
+    const currentHour = moment(now.format('H'), format);
+
+    if(currentHour.isBefore(timeblockHour)) {
+        $(this).addClass('future');
+    } else if(currentHour.isAfter(timeblockHour)) {
+        $(this).addClass('past');
+    } else {
+        $(this).addClass('present');
+    }
+});
 
