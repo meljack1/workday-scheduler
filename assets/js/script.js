baf07df6d2c0f3980ef8be57370e3a418d866d89
Mel Jack <melissa_jack@hotmail.co.uk>
2021-07-02 01:10:07 +0100

    Store text area data locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const currentDayEl = $('#currentDay');
+const buttonList = $('button');
+const timeblockList = $('textarea');
 
 const now = moment();
 currentDayEl.text(now.format("dddd, MMMM Do"));
+
+// Change colour of textbox for past/present/future times
 
 $("textarea").each(function() {
     const format = 'H';
@@ -20,3 +24,20 @@ $("textarea").each(function() {
     }
 });
 
+// If button clicked, store value of textbox locally under id number of the div
+
+buttonList.on('click', function() {
+    const textBox = $(this).prev();    
+    // Get id number of the parent div
+    const id = $(this).parent().attr('id');
+    localStorage.setItem(id, textBox.val());
+});
+
+// Display value of text area in text area
+
+(function() {
+    for(let i = 0; i <= timeblockList.length; i++) {
+        const id = $(timeblockList[i]).parent().attr('id');
+        $(timeblockList[i]).val(localStorage.getItem(id));
+    }
+})(); 
